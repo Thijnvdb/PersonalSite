dc32d9dd0467662772f5b48433fadb196772334b
Thijnvdb <thijnvandenbergh@gmail.com>
2022-02-16 00:33:07 +0100

    minor fixes

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Divider from './Divider'
 import style from "../styles/Contact.module.scss";
+import { faPhone, faAt } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function Contact() {
     return (<>
         <Divider title="contact"/>
         <div className='w-5/6 flex items-center flex-col'>
-            <a className={style.link} href="mailto:thijnvandenbergh@gmail.com">Thijnvandenbergh@gmail.com</a>
-            <a className={style.link} href="tel:+31636589487">+31 6 365 89 487</a>
+            <a className={style.link} href="mailto:thijnvandenbergh@gmail.com"><FontAwesomeIcon className='h-8' icon={faAt}/> <span className='mx-auto'>Thijnvandenbergh@gmail.com</span></a>
+            <a className={style.link} href="tel:+31636589487"><FontAwesomeIcon className='h-8' icon={faPhone}/>  <span className='mx-auto'>+31 6 365 89 487</span></a>
         </div>
     </>)
 }
